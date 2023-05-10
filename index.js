affd7d3063a62ea82abd7cf1794dbced76a68745
nikhitkumar <nikhitkumar00@gmail.com>
2023-05-10 14:28:51 +0530

    made the javascripting more efficient

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
-function add() {
+function init(process) {
     var n1 = parseFloat(document.getElementById("num1").value);
     var n2 = parseFloat(document.getElementById("num2").value);
+    (process === 1) ? add(n1, n2) : (process === 2) ? subtract(n1, n2) : (process === 3) ? multiply(n1, n2) : (process === 4) ? divide(n1, n2) : exponent(n1, n2);
+}
+
+function add(n1,n2) {
     var result = n1 + n2;
     check(result);
 }
 
-function subtract() {
-    var n1 = parseFloat(document.getElementById("num1").value);
-    var n2 = parseFloat(document.getElementById("num2").value);
+function subtract(n1,n2) {
     var result = n1 - n2;
     check(result);
 }
 
-function multiply() {
-    var n1 = parseFloat(document.getElementById("num1").value);
-    var n2 = parseFloat(document.getElementById("num2").value);
+function multiply(n1,n2) {
     var result = n1 * n2;
     check(result);
 }
 
-function divide() {
-    var n1 = parseFloat(document.getElementById("num1").value);
-    var n2 = parseFloat(document.getElementById("num2").value);
+function divide(n1, n2) {
+    if (n2 == 0) {
+        document.getElementById("answer").innerText = "Division By Zero not Possible";
+        alert("Division By Zero not Possible");
+        return;
+    }
     var result = n1 / n2;
     check(result);
 }
 
-function exponent() {
-    var n1 = parseFloat(document.getElementById("num1").value);
-    var n2 = parseFloat(document.getElementById("num2").value);
+function exponent(n1,n2) {
     var result = n1 ** n2;
     check(result);
 }
 
 function check(result) {
     if (isNaN(result)) {
-        alert("Enter Valid Number");
         document.getElementById("answer").innerText = "Invalid Input";
+        alert("Enter Valid Number");
         return;
     }
     document.getElementById("answer").innerText = result;
